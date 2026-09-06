8dd2469e1e16bdb5bc10efe0f47cb32e754623f4
Muneeb amin <ma2586422@gmail.com>
2026-09-06 18:23:11 +0500

    fix: separate MEDIA_URL from STATIC_URL to fix ImproperlyConfigured on collectstatic

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -184,8 +184,8 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
-MEDIA_URL = "/static/images/"
-MEDIA_ROOT = BASE_DIR / "static/images"
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
 
 STORAGES = {
     "default": {
